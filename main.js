9caf7e01b898e9d6c4ace57d8c7afa772d316a96
sarthakmisra <78964203+sarthakmisra@users.noreply.github.com>
2022-02-09 15:22:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,10 +4,27 @@ function preload() {
 function setup() {
     canvas = createCanvas(450,450);
     canvas.center();
+    webcam = createCapture(VIDEO);
+   webcam.size(0,0,450,450);
+    webcam.hide();
+
+    posenet = ml5.poseNet(webcam ,modelloaded);
+    posenet.on('gotposes', gotposes);
 }
 function draw() {
-
+image(webcam,0,0,450,450);
 }
 function take() {
     save("Image.png");
+}
+function modelloaded() {
+console.log('model is loaded');
+}
+function gotposes(results) {
+    if (results.length > 0)
+    {
+        console.log(results);
+        console.log("nose x =" + results[0].pose.nose.x);
+        console.log("nose y =" + results[0].pose.nose.y);  
+    }
 }
