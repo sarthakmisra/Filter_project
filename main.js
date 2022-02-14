c251daa97d35b81d4bb801a31f120a06f6de2caa
sarthakmisra <78964203+sarthakmisra@users.noreply.github.com>
2022-02-14 17:43:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-function preload() {
+variable_A = 0;
+variable_B = 0;
 
+function preload() {
+ filtar = loadimage("https://i.postimg.cc/76DHVwx0/lipstick-removebg-preview.png");//this variable is the lipstick now!
 }
 function setup() {
     canvas = createCanvas(450,450);
@@ -13,6 +16,7 @@ function setup() {
 }
 function draw() {
 image(webcam,0,0,450,450);
+image(filtar,variable_A,variable_B,0,20);//this piece of code brings the lipstick on the canvas.
 }
 function take() {
     save("Image.png");
@@ -23,8 +27,10 @@ console.log('model is loaded');
 function gotposes(results) {
     if (results.length > 0)
     {
+        variable_A = results[0].pose.nose.x;
+        variable_B = results[0].pose.nose.y;
         console.log(results);
-        console.log("nose x =" + results[0].pose.nose.x);
-        console.log("nose y =" + results[0].pose.nose.y);  
+        console.log("nose x =" + variable_A);
+        console.log("nose y =" + variable_B);  
     }
 }
