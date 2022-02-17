25395ea857d53e6a305fe9b7c3a83a51c0b183b9
sarthakmisra <78964203+sarthakmisra@users.noreply.github.com>
2022-02-17 15:22:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,21 +2,23 @@ variable_A = 0;
 variable_B = 0;
 
 function preload() {
- filtar = loadimage("https://i.postimg.cc/76DHVwx0/lipstick-removebg-preview.png");//this variable is the lipstick now!
+ filtar = loadImage("https://i.postimg.cc/76DHVwx0/lipstick-removebg-preview.png");//this variable is the lipstick now!
 }
 function setup() {
-    canvas = createCanvas(30,30,450,450);
+    canvas = createCanvas(300,300);
     canvas.center();
+    canvas.position(530,200);
     webcam = createCapture(VIDEO);
-   webcam.size(0,0,450,450);
+   webcam.size(450,450);
     webcam.hide();
+    
 
     posenet = ml5.poseNet(webcam ,modelloaded);
-    posenet.on('gotposes', gotposes);
+    posenet.on('pose', gotposes);
 }
 function draw() {
-image(webcam,0,0,450,450);
-image(filtar,variable_A,variable_B,0,20);//this piece of code brings the lipstick on the canvas.
+image(webcam,0,0,300,300);
+image(filtar , variable_A , variable_B , 40 , 40);//this piece of code brings the lipstick on the canvas.
 }
 function take() {
     save("Image.png");
@@ -27,9 +29,9 @@ console.log('model is loaded');
 function gotposes(results) {
     if (results.length > 0)
     {
-        variable_A = results[0].pose.nose.x;
-        variable_B = results[0].pose.nose.y;
         console.log(results);
+        variable_A = results[0].pose.nose.x -80;
+        variable_B = results[0].pose.nose.y -70;
         console.log("nose x =" + variable_A);
         console.log("nose y =" + variable_B);  
     }
